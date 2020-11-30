a98a04af57d4c977a90b91a6a649ece21738d0b9
sc <sotyo@icloud.com>
2020-11-30 13:08:38 +0900

    wheels@2

--- app.js
+++ app.js
@@ -3,9 +3,24 @@
 /**
  * Examples
  */
-require('./src/utils/readCsv')
-    .readbyline({interval: 10, filePath: "data/bf_short_1s.csv"}, "price","bid_price","ask_price","v_bid","v_ask")
-    .then(data => console.log(data))
-require('./example/work/index')
-    .work("hello world", "javascrpt", "quant")
-    .then(data => console.log(data))
+
+// require('./src/utils/readCsv')
+//     .readbyline({interval: 10, filePath: "data/bf_short_1s.csv"}, "price","bid_price","ask_price","v_bid","v_ask")
+//     .then(data => console.log(data))
+
+// require('./example/work/index')
+//     .work("hello world", "javascrpt", "quant")
+//     .then(data => console.log(data))
+
+// const { regularize } = require('./src/utils/calculate')
+// const target = [2,3,4,7,4,1,3,2,3,5,6,7,8,9,1,23]
+// const matrix = require('./src/core/bayesian_calculate').generateTimeSeries({len: 3}, ...target)
+// console.log(regularize(matrix[0], 3))
+
+// const { generateTimeSeries, kmeansClusters } = require('./src/core/bayesian_calculate')
+// const ts = generateTimeSeries({len: 3, interval: 1}, 1,3,4,12,45,6,3,23,62,8,5,8,2,21,3,454,5,45,345,435,44,324,34,6,6,6,54,65,64,54,453,54,53,5,55,65,76,687,5,32,43,4,64,564,53,42,42,43,44,65,33,67,85,65,54,453,3,1)
+// kmeansClusters(ts, 4).then(data => data.map(cluster => console.log(cluster.centroid)))
+
+// const s = [[21,3,44,10], [3,42,9,20]]
+// console.log(require('./src/core/bayesian_calculate').predictDpiBySimilarity([1,2,3], s, 1))
+// console.log(require('./src/core/bayesian_calculate').predictDpiByPoission([1,2,3], s))
--- src/utils/calculate.js
+++ src/utils/calculate.js
@@ -1,17 +1,56 @@
-function norm(array, len = array.length) {
+'use strict'
+const math = require('mathjs');
+
+/**
+ * normalize
+ * @param {Array} array 
+ * @param {Number} len first * elements in the array which needs to be normalized
+ */
+function normalize(array, len = array.length) {
     if (len > array.length) len = array.length
     const sliced = len == array.len ? array : array.slice(0, len)
-    const after = array.slice(len)
-    const mean = sliced.reduce((acc, val) => acc + val, 0)/len
-    const std = stddev(sliced)
-    return sliced.reduce((acc, val) => acc.concat((val - mean)/std), []).concat(after)
+    const max = Math.max(...array)
+    const min = Math.min(...array)
+    return sliced.reduce((acc, val) => acc.concat((val - min)/(max - min)), []).concat(array.slice(len))
 }
 
+/**
+ * regularize so the mean = 0 and std =1
+ * @param {Array} array 
+ * @param {Number} len first * elements in the array which needs to be normalized
+ */
+function regularize(array, len = array.length) {
+    if (len > array.length) len = array.length
+    const sliced = len == array.len ? array : array.slice(0, len)
+    const mean = sliced.reduce((acc, val) => acc + val, 0)/len
+    const std = stddev(sliced)
+    return sliced.reduce((acc, val) => acc.concat((val - mean)/std), []).concat(array.slice(len))
+}
+
+/**
+ * calculate 2D norm of diffs of a and b
+ * @param {Array} a 
+ * @param {Array} b 
+ */
+function norm(a, b) {
+    const diff = a.reduce((acc, val, index) => acc.concat(val - b[index]), [])
+    return math.norm(diff) ** 2
+}
+
+/**
+ * calculate standard error of an array
+ * @param {Array} array 
+ */
 function stddev(array) {
-    const mean = array.reduce((acc, val) => acc + val, 0)/arr.length
+    const mean = array.reduce((acc, val) => acc + val, 0)/array.length
     return Math.sqrt(array.reduce((acc, val) => acc.concat((val - mean) ** 2), []).reduce((acc, val) => acc + val, 0)/array.length)
 };
 
+/**
+ * slice an array into equal-length arrays
+ * @param {Array} array target
+ * @param {Number} slice number of slices
+ */
 function split(array, slice) {
     const count = parseInt(array.length/slice)
     const length = array.length % slice == 0 ? count : count + 1
@@ -23,6 +62,30 @@ function split(array, slice) {
     return results
 }
 
+/**
+ * Similarity of two arrays
+ * @param {Array} x 
+ * @param {Array} y 
+ */
+function similarity(x, y) {
+    const y_x = y.slice(0, x.length)
+    const len = x.length
+    let num = 0
+    const den = len * stddev(x) * stddev(y_x)
+    const x_m = x.reduce((acc, val) => acc + val)/len
+    const y_m = y_x.reduce((acc, val) => acc + val)/len
+    for (let i = 0; i < len; i ++) {
+        num += (x[i] - x_m) * (y_x[i] - y_m)
+    }
+    return num/den
+}
+
+/**
+ * slice an array with fixed intervals and form the resulted arrays into a 2D matrix
+ * @param {Array} array 
+ * @param {Number} dataLength length of slices
+ * @param {Number} interval slice interval
+ */
 function generateSeryMatrix(array, dataLength, interval = 1) {
     const matrix = []
     for (let i = 0; i < parseInt((array.length - dataLength)/interval + 1); i ++) {
@@ -31,4 +94,4 @@ function generateSeryMatrix(array, dataLength, interval = 1) {
     return matrix
 }
 
-module.exports = { norm, stddev, split, generateSeryMatrix }
+module.exports = { norm, normalize, regularize, similarity, stddev, split, generateSeryMatrix }
